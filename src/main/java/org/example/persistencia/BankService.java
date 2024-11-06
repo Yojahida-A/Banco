676406b8d84752ca7278b21d897fb91b5b08dcb2
Yojahida Mónica <45293103@continental.edu.pe>
2024-11-05 22:14:34 -0500

    Clase BankService de la capa ersistencia

---
+++ src/main/java/org/example/persistencia/BankService.java
@@ -0,0 +1,11 @@
+package org.example.persistencia;
+
+import org.example.logica.Client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class BankService {
+    private Map<String, Client> clients = new HashMap<>();
+}
